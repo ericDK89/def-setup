cc64e970951cf9aec0f77b94dce9bdb7c2c46496
Eric Macedo Gomes <ericthr42@gmail.com>
2024-03-16 16:59:52 -0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,31 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+set SPACEFISH_PROMPT_ADD_NEWLINE false
+
+starship init fish | source
+
+# Aliases
+# alias cat="bat --theme=$(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)"
+alias python='python3'
+alias pip='pip3'
+
+alias ls='lsd'
+
+alias tm='tmux'
+alias tma='tmux attach'
+alias tmd='tmux detach'
+alias tml='tmux ls'
+alias tmkw='tmux kill-window -t'
+alias tmks='tmux kill-session -t'
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
+
+# added by Webi for pyenv
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
