885986f5ab49cf08b5af1d5943104f14a3aec8a1
Eric Macedo Gomes <ericthr42@gmail.com>
2024-03-25 23:23:23 -0300

    Add files via upload

---
+++ nvim - LazyVIm 2/lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  { "RRethy/vim-illuminate", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+}
