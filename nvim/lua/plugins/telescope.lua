c21325da1985798d44ce739b1a9e06dff200be1c
Eric Macedo Gomes <ericthr42@gmail.com>
2024-04-08 08:42:34 -0300

    Add files via upload

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    { "<C-f>",            "<cmd>Telescope live_grep<cr>",  desc = "Search grep" },
+    { "<leader><leader>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+  },
+}
